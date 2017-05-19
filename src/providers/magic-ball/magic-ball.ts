ff92daa417e0a70d2939b47954e51851d380e2b2
Pardiez <david@davidpena.es>
2017-05-19 16:36:06 +0200

    add logic to magic ball provider

--- src/providers/magic-ball/magic-ball.ts
+++ src/providers/magic-ball/magic-ball.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
-import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the MagicBallProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class MagicBallProvider {
 
-  constructor(public http: Http) {
-    console.log('Hello MagicBallProvider Provider');
+  answers: Array<string> = [
+    'Yes',
+    'No',
+    'Maybe',
+    'All signs point to yes',
+    'Try again later',
+    'Without a doubt',
+    'Don\'t count on it',
+    'Most likely',
+    'Absolutely not'
+  ];
+
+  constructor() {
   }
 
+  getAnswers(){
+    return this.answers
+  }
+
+  getRandomAnswer(){
+    let position = this.getRandomInt(0, this.answers.length-1);
+    return this.answers[position]
+  }
+
+  getRandomInt(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
