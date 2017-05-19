d79981365763a8feb86f0dc550e444ec5f08a024
Pardiez <david@davidpena.es>
2017-05-19 16:49:59 +0200

    add magicBall to home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,25 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { MagicBallProvider } from '../../providers/magic-ball/magic-ball';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  title: string = 'My Page'
+  answer: any = "..."
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public magicBall: MagicBallProvider
+  ) { }
 
+  changeTitle(title) {
+    this.title = title
   }
 
+  showAnswer(){
+    this.answer = this.magicBall.getRandomAnswer();
+  }
 }
